72d4336fdb66c16ccb553a2f8120004e08ab801d
Billy Khaled <billy.codes@gmail.com>
2019-07-11 17:23:03 +0800

    Added files with layouts

--- includes/header.php
+++ includes/header.php
@@ -1,15 +1,31 @@
+<?php include 'db.php';?>
 <!DOCTYPE html>
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<!--[if gt IE 8]><!--> 
+<html class="no-js"> 
+<!--<![endif]-->
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <title>jShop - Jewelry Store</title>
         <meta name="description" content="">
+        <!--Let browser know website is optimized for mobile-->
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="">
+
+        <link rel="icon" href="src/img/icon.png">      
+        <!-- font-awesome -->
+        <link rel="stylesheet" href="src/css/font-awesome-4.6.3/css/font-awesome.min.css">
+        <!--Import materialize.css-->
+        <link type="text/css" rel="stylesheet" href="src/css/materialize.min.css"  media="screen,projection"/>
+        <!-- animate css -->
+        <link rel="stylesheet" href="src/css/animate.css-master/animate.min.css">
+        <!-- Custom styles -->
+        <link rel="stylesheet" href="src/css/style.css">
+        <!-- Progress bar -->
+        <link rel='stylesheet' href='src/css/nprogress.css'/>
+        
     </head>
     <body>
         <!--[if lt IE 7]>
